10a377b1738aa39c670b6d64f35674d444709679
IlyesHernandez <ilyeshernandez.pro@gmail.com>
2022-05-21 13:59:11 +0200

    first-commit-tamplate

---
+++ app.js
@@ -0,0 +1,9 @@
+const kieryjs = require('kieryjs');
+
+// create the server
+kieryjs.createServer({
+    index: 'index.ejs', // render the file in "views/index.ejs"
+    varPage: { HelloWorld: "Hello, World!" }, // Add a variable called "HelloWorld" with the value "Hello, World!"
+    port: 3000, // at port 3000 (http://localhost:3000)
+    staticFolder: 'public' // set static folder (css, js, img) to "public"
+});
