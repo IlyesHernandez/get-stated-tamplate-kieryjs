51e32f21127b1a9d595150694eb459355a8df287
IlyesHernandez <ilyeshernandez.pro@gmail.com>
2022-05-21 21:35:28 +0200

    1.1.1v Update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const kieryjs = require('kieryjs');
+const kieryjs = require('kieryjs'); // import KieryJS with the version 1.1.1
 
 // create the server
 kieryjs.createServer({
-    index: 'index.ejs', // render the file in "views/index.ejs"
+    index: 'index.ejs', // render the file "index.ejs"
+    includeVariableOnPage: true, // if you must to include variable on index.ejs then leash true 
+    engine: 'ejs', // set the view engine you want to use (ejs, md)
     varPage: { HelloWorld: "Hello, World!" }, // Add a variable called "HelloWorld" with the value "Hello, World!"
-    port: 3000, // at port 3000 (http://localhost:3000)
+    port: 3000, // open on the port 3000 (http://localhost:3000)
     staticFolder: 'public' // set static folder (css, js, img) to "public"
-});
-
-kieryjs.app.listen(8080)
+});     
